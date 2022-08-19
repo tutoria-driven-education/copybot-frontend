624c8c877f081d7b510429efc59ae1e9f9495288
Viviane Brenner <brenner.vivi@gmail.com>
2022-08-19 14:30:30 -0300

    styled logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 import { IoArrowBackCircleSharp } from "react-icons/io5";
+import logoDriven from "/public/logoDriven.png";
 
 const Navbar = () => {
   return (
     <NavbarStyle>
+      <img src={logoDriven} alt="Logo Driven" />
       <BackButton>
         <IoArrowBackCircleSharp size={30} />
       </BackButton>
@@ -16,6 +18,16 @@ export default Navbar;
 const NavbarStyle = styled.nav`
   padding: 24px;
   background-color: #45455c;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
-const BackButton = styled.div``;
+const BackButton = styled.div`
+  position: absolute;
+  top: 23px;
+  left: 30px;
+  width: 30px;
+  height: 25px;
+`;
+
